dfaa17f50ee860537118bb87ba477c2a2986ebef
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-08-07 20:48:26 +0900

    [update]的の追加 [Fix]フォルダーの整理

--- Assets/Scripts/Player/TargetController.cs
+++ Assets/Scripts/Player/TargetController.cs
@@ -7,6 +7,10 @@ public class TargetController : InputBase
     /// <summary>ターゲットカーソルの移動の倍率</summary>
     [SerializeField] float _magnification = 1f;   
 
+    Vector2 _pos = Vector2.zero;
+    bool _canLook = false;
+    bool _isLook = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +22,7 @@ public class TargetController : InputBase
     {
         Vector2 input = _inputController.Player.PointCannonStick.ReadValue<Vector2>();
         //Vector2 input = _inputController.Player.PointCannonMouse.ReadValue<Vector2>();
+        //_pos = this.transform.localPosition;
 
         // 入力が無いときは位置を維持する
         if (input == Vector2.zero) 
@@ -30,13 +35,41 @@ public class TargetController : InputBase
             //this.transform.position = input;
         }
 
-        if (_inputController.Player.PointResetStick.triggered)
+        //float dirX = this.transform.localPosition.x - input.x * _magnification;
+        //float dirY = this.transform.localPosition.y - input.y * _magnification;
+
+        //if (dirX != 0 && dirY != 0)
+        //{
+        //    _pos.x += dirX * 0.1f;
+        //    _pos.y += dirY * 0.1f;
+        //}
+
+        if (_inputController.Player.PointResetStick.triggered && !_canLook)
         {
             this.transform.localPosition = Vector2.right * _magnification;
         }
-        //else if (ロックオンしている時)
-        //{
+    }
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //_canLook = true;
+        //Debug.Log(_canLook);
+
+        //if (_inputController.Player.PointResetStick.triggered && !_isLook)
+        //{
+        //    this.transform.position = collision.transform.position;
+        //    _isLook = true;
         //}
+        //else if (_inputController.Player.PointResetStick.triggered && _isLook)
+        //{
+        //    this.transform.localPosition = Vector2.right * _magnification;
+        //    _isLook = false;
+        //}
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //_canLook = false;
+        //_isLook = false;
     }
 }
