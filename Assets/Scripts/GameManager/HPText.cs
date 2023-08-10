1339ff46e55e6cc9b36ab1553bd40ff69fc2f120
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-08-10 22:44:35 +0900

    [add]敵のHPのUIを追加しました。

--- Assets/Scripts/GameManager/HPText.cs
+++ Assets/Scripts/GameManager/HPText.cs
@@ -7,20 +7,23 @@ public class HPText : MonoBehaviour
 {
     [SerializeField] Text _hpText = null;
     [SerializeField] float _hp = 1;
-    [SerializeField] CharacterBase _player = null;
+    [SerializeField] GameObject _player = null;
 
     // Start is called before the first frame update
     void Start()
     {
         _hpText = GetComponent<Text>();
-        _player = GetComponent<CharacterBase>();
-        _hp = _player.CharacterHp;
+        _hp = _player.GetComponent<CharacterBase>().CharacterHp;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _hp = _player.CharacterHp;
-        _hpText.text = _hp.ToString($"HP : {_hp}");
+        if (_hp > 0) 
+        {
+            _hp = _player.GetComponent<CharacterBase>().CharacterHp;
+        }
+
+        _hpText.text = "HP:" + _hp.ToString("F0");
     }
 }
