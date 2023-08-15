178316170314b07db034a52be84a0c220304ba78
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-08-15 23:55:46 +0900

    [add]メニュー画面の制作を始めました。

---
+++ Assets/Scripts/Player/Menu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.XInput;
+
+public class Menu : InputBase
+{
+    [SerializeField] GameObject _menuCanvas = null;
+    [SerializeField] GameObject _itemPanel = null;
+    [SerializeField] GameObject _documentPanel = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _menuCanvas.SetActive(false);
+        _itemPanel.SetActive(false);
+        _documentPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_inputController.Player.Crouch.triggered)
+        {
+            _menuCanvas.SetActive(!_menuCanvas);
+        }
+    }
+}
