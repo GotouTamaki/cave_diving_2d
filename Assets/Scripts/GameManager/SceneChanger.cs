12396d4e3cef6440ae08cb408b2705c77a2c44f9
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-09-16 21:51:16 +0900

    [update]フェードの処理の不要なものの削除

--- Assets/Scripts/GameManager/SceneChanger.cs
+++ Assets/Scripts/GameManager/SceneChanger.cs
@@ -6,26 +6,21 @@ using DG.Tweening;
 public class SceneChanger : MonoBehaviour
 {
     /// <summary>フェードインパネル</summary>
-    [SerializeField] Image _fadeInPanel = null;
-    /// <summary>フェードアウトパネル</summary>
-    [SerializeField] Image _fadeOutPanel = null;
+    [SerializeField] Image _fadePanel = null;
     /// <summary>フェードのインターバル</summary>
     [SerializeField] float _interval = 5f;
-    ///// <summary>フェードモード 0がフェードイン1がフェードアウト</summary>
-    //[Tooltip("フェードモード\n0がフェードイン1がフェードアウト")]
-    //[SerializeField] int _fadeMode = 0;
 
     /// <summary>引数に入力された名前のシーンに遷移します</summary>
     /// <param name="sceneName">遷移するシーンの名前</param>
     public void SceneChangeFade(string sceneName)
     {
-        _fadeInPanel.gameObject.SetActive(true);
-        _fadeInPanel.DOFade(1, _interval)
+        // フェードモード 0がフェードイン 1がフェードアウト
+        _fadePanel.gameObject.SetActive(true);
+        _fadePanel.DOFade(1, _interval)
             .OnComplete(() =>
             {
                 SceneManager.LoadScene(sceneName);
-                _fadeInPanel.gameObject.SetActive(true);
-                _fadeInPanel.DOFade(0, _interval).OnComplete(() => _fadeInPanel.gameObject.SetActive(false));
+                _fadePanel.DOFade(0, _interval).OnComplete(() => _fadePanel.gameObject.SetActive(false));
             });
     }
 }
