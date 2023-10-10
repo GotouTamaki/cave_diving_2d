5b9ebee1abd91cec8b13f1e7684ced6ed1abf061
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-10-10 15:59:45 +0900

    [update]弾のメニューの開閉機能の追加

--- Assets/Scripts/Bullet/BulletsMenu.cs
+++ Assets/Scripts/Bullet/BulletsMenu.cs
@@ -7,11 +7,15 @@ public class BulletsMenu : InputBase
 {
     [SerializeField] List<BulletBase> _bullets = new ();
     [SerializeField] float _moveTime = 1f;
+    [SerializeField] float _fadeTime = 1f;
     [SerializeField] Image[] _images = null;
+
+    CanvasGroup _canvasGroup;
 
     void Start()
     {
         _images = GetComponentsInChildren<Image> ();
+        _canvasGroup = GetComponent<CanvasGroup> ();
     }
 
     void Update()
@@ -19,6 +23,10 @@ public class BulletsMenu : InputBase
         if (_inputController.Player.BulletChangeL.triggered)// 押したことを判定
         {
             UpdatePosition();
+        }
+        else if(_inputController.Player.BulletChangeL.WasReleasedThisFrame())
+        {
+            ResetPosition();
         }
     }
 
@@ -38,10 +46,22 @@ public class BulletsMenu : InputBase
             rectPos.x = pos.x;
             rectPos.y = pos.y;
             rt.DOAnchorPos(new Vector2(pos.x, pos.y), _moveTime);
+            _canvasGroup.DOFade(1, _fadeTime);
             //rt.anchoredPosition = pos;
 
             // 次の位置を時計回りに回転
             pos = Quaternion.Euler(0, 0, -angle) * rectPos;
+        }
+    }
+
+    void ResetPosition()
+    {
+        for (int i = 0; i < _bullets.Count; i++)
+        {
+            // 位置をセット
+            RectTransform rt = _images[i].rectTransform;
+            rt.DOAnchorPos(Vector2.zero, _moveTime);
+            _canvasGroup.DOFade(0, _fadeTime);
         }
     }
 }
